25fd7b458e27ef92b1b528acca9847532ff5ec3e
sineadmcd <s.mcdonagh24@nuigalway.ie>
2017-04-06 10:05:45 +0100

    bugs

--- MM2example/src/testingCPP.cpp
+++ MM2example/src/testingCPP.cpp
@@ -1,13 +1,12 @@
 int main(){
 
-    double d=1.0/2;
+    double d=1/2;
     
     int i=3;
     int j=4;
-    
-    if(3=4)
+
       printf("hello world!");
       
-      i=i+1; \n return i;++;
+     return i++;
       
       }
